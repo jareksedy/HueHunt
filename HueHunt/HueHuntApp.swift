455d1cb9f198f8f9d8ae1b0fa451d5468cd8d24f
Yaroslav Sedyshev <yaroslavse@halykbank.kz>
2023-10-13 10:59:31 +0600

    Initial Commit

---
+++ HueHunt/HueHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  HueHuntApp.swift
+//  HueHunt
+//
+//  Created by Ярослав on 13.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HueHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
